b5f50b2eeb61a1e67c01a8429009fff54ebb3316
opportunityplanet <opportunityplanet.info@gmail.com>
2026-05-02 09:39:15 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,20 @@
-const CACHE = 'op-tracker-v1';
-const FILES = [
-  '/op-tracker/',
-  '/op-tracker/index.html',
-  '/op-tracker/manifest.json',
-  '/op-tracker/icon-192.png',
-  '/op-tracker/icon-512.png'
-];
+const CACHE = 'op-tracker-v4';
+const ASSETS = ['./', './index.html'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting())
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-    )).then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/op-tracker/')))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
   );
 });
